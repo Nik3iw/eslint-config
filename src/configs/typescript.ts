e6fd46e5aa45e445d5e3729d5ed920783f8bcb36
Nik <nik3iw@gmail.com>
2026-06-02 18:38:36 +0330

    add typescript config

---
+++ src/configs/typescript.ts
@@ -0,0 +1,52 @@
+import type { Config } from "eslint/config";
+import { defineConfig } from "eslint/config";
+import tsEslint from "typescript-eslint";
+import type { ConfigWithExtends } from "typescript-eslint";
+
+interface Params {
+  tsconfigRootDir?: string;
+}
+
+export function typescriptConfig(params: Params): Config[] {
+  return defineConfig(
+    tsEslint.configs.strictTypeChecked,
+    tsEslint.configs.stylisticTypeChecked,
+
+    {
+      languageOptions: {
+        parserOptions: {
+          projectService: true,
+          ...(params.tsconfigRootDir !== undefined
+            ? { tsconfigRootDir: params.tsconfigRootDir }
+            : {}),
+        },
+      } satisfies ConfigWithExtends["languageOptions"],
+    },
+
+    // Rules
+    {
+      files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+      rules: {
+        "@typescript-eslint/prefer-optional-chain": ["off"],
+        "@typescript-eslint/prefer-nullish-coalescing": ["off"],
+
+        "no-unused-vars": ["off"],
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            vars: "all",
+            args: "all",
+            argsIgnorePattern: "^_",
+            caughtErrors: "all",
+            caughtErrorsIgnorePattern: "^ignore",
+            destructuredArrayIgnorePattern: "^_",
+            ignoreRestSiblings: false,
+            ignoreClassWithStaticInitBlock: false,
+            ignoreUsingDeclarations: false,
+            reportUsedIgnorePattern: false,
+          },
+        ],
+      },
+    },
+  );
+}
