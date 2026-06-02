c6c478ade089649aaa81910c13fa4b70cc2c5ccd
Nik <nik3iw@gmail.com>
2026-06-02 18:39:01 +0330

    add unicorn rules

---
+++ src/configs/unicorn.ts
@@ -0,0 +1,19 @@
+import type { Config } from "eslint/config";
+import { defineConfig } from "eslint/config";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+
+export function unicornConfig(): Config[] {
+  return defineConfig(
+    eslintPluginUnicorn.configs.recommended,
+
+    // Rules
+    {
+      files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+      rules: {
+        "unicorn/filename-case": ["off"],
+        "unicorn/no-null": ["off"],
+        "unicorn/prevent-abbreviations": ["off"],
+      },
+    },
+  );
+}
